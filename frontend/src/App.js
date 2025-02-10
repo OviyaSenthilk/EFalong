d040b46831a13cc4e3b003758926ec72e2b07432
OviyaSenthilk <oviya.s2604@gmail.com>
2025-02-10 16:51:32 +0530

    milestone_6

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {LoginPage,SignupPage} from "./routes/routes";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        {/* <Route path ="/" element={<Home/>}/> */}
+        <Route path ='/login' element={<LoginPage/>}/>
+        <Route path ='/signup' element={<SignupPage/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
